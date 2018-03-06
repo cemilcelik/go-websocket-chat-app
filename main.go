da3c928e1297987e73d1964e3f5fb4dd4496578c
cemilcelik <cemilcelik84@gmail.com>
2018-03-06 15:42:29 +0300

    Add handleConnections function to listen channel and handle connection.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -21,12 +22,34 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
 func main() {
 	fmt.Println("App started.")
 
 	fs := http.FileServer(http.Dir("public"))
 
 	http.Handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
 
 	fmt.Println("Http server is running on http://localhost:8000")
 	err := http.ListenAndServe("localhost:8000", fs)
